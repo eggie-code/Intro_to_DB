4626d8a87c674fc400a96f8ddca9c3d5a2dba713
Abdulhabib junis <fatabdi1234@gmail.com>
2025-08-13 16:16:58 +0300

    task_2

--- task_2.sql
+++ task_2.sql
@@ -1,8 +1,7 @@
 --create authors table
 CREATE TABLE IF NOT EXISTS authors (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
+    authors_name VARCHAR(50) NOT NULL,
     birthdate DATE,
     nationality VARCHAR(50)
 );
