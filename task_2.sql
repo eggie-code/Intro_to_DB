367abd6f27656342dd7f029659c843ea14ab2ec1
Abdulhabib junis <fatabdi1234@gmail.com>
2025-08-13 16:28:27 +0300

    task_2

--- task_2.sql
+++ task_2.sql
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS Orders (
 
 -- Create Order_details table
 CREATE TABLE IF NOT EXISTS Order_details (
-    order_details_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_details INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
