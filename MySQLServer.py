3b6316aab607afe85b4ec471bfb3007b8895d8f6
Abdulhabib junis <fatabdi1234@gmail.com>
2025-08-13 15:12:19 +0300

    MySQLServer

--- MySQLServer.py
+++ MySQLServer.py
@@ -15,15 +15,15 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' 'created successfully!")
 
-
-except Error as e:
+    except Error as e:
     print(f"Error: {e}")
 
-finally:
+    finally:
     if 'cursor' in locals():
         cursor.close()
     if 'connection' in locals() and connection.is_connected():
         connection.close()
+
 
 if __name__ == "__main__":
     create_database()
